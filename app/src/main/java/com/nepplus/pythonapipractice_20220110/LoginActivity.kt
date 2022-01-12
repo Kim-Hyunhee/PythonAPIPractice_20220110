dce6fffeb2db1aa41128433bc262fe040ce2eee4
김현희 <tta_bi@naver.com>
2022-01-12 09:56:40 +0900

    메인 화면 / 연락처 등록 화면 추가

---
+++ app/src/main/java/com/nepplus/pythonapipractice_20220110/LoginActivity.kt
@@ -0,0 +1,70 @@
+package com.nepplus.pythonapipractice_20220110
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import com.nepplus.pythonapipractice_20220110.models.BasicResponse
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.edtEmail
+import kotlinx.android.synthetic.main.activity_sign_up.edtPassword
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnSignUp.setOnClickListener {
+            val myIntent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(myIntent)
+        }
+
+        btnLogin.setOnClickListener {
+
+            val inputId = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+
+            apiList.getRequestLogin(inputId, inputPw).enqueue(object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                    if (response.isSuccessful) {
+
+                        val br = response.body()!!
+
+                        Toast.makeText(
+                            mContext,
+                            "${br.data.user.nickname}님 환영합니다!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        val myIntent = Intent(mContext, MainActivity::class.java)
+                        startActivity(myIntent)
+                        finish()
+
+                    }
+                    else {
+
+                        val jsonObj = JSONObject( response.errorBody()!!.string() )
+
+                        Toast.makeText(mContext, jsonObj.getString("message"), Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
+
+        }
+
+    }
+}
--- app/src/main/java/com/nepplus/pythonapipractice_20220110/MainActivity.kt
+++ app/src/main/java/com/nepplus/pythonapipractice_20220110/MainActivity.kt
@@ -3,66 +3,16 @@ package com.nepplus.pythonapipractice_20220110
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.nepplus.pythonapipractice_20220110.models.BasicResponse
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.edtEmail
-import kotlinx.android.synthetic.main.activity_main.edtPassword
-import kotlinx.android.synthetic.main.activity_sign_up.*
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSignUp.setOnClickListener {
-            val myIntent = Intent(mContext, SignUpActivity::class.java)
+        btnAddContact.setOnClickListener {
+            val myIntent = Intent(mContext, EditContactActivity::class.java)
             startActivity(myIntent)
         }
-
-        btnLogin.setOnClickListener {
-
-            val inputId = edtEmail.text.toString()
-            val inputPw = edtPassword.text.toString()
-
-            apiList.getRequestLogin(inputId, inputPw).enqueue(object : Callback<BasicResponse> {
-                override fun onResponse(
-                    call: Call<BasicResponse>,
-                    response: Response<BasicResponse>
-                ) {
-
-                    if (response.isSuccessful) {
-
-                        val br = response.body()!!
-
-                        Toast.makeText(
-                            mContext,
-                            "${br.data.user.nickname}님 환영합니다!",
-                            Toast.LENGTH_SHORT
-                        ).show()
-
-                    }
-                    else {
-
-                        val jsonObj = JSONObject( response.errorBody()!!.string() )
-
-                        Toast.makeText(mContext, jsonObj.getString("message"), Toast.LENGTH_SHORT).show()
-
-                    }
-
-                }
-
-                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
-
-                }
-
-            })
-
-        }
-
     }
 }
